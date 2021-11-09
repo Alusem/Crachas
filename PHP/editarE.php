24ead708fc43e7f87d5f6f6258cfb8df683b21e3
Alusem <58275463+Alusem@users.noreply.github.com>
2021-11-09 09:17:27 -0300

    Commit do projeto local
    projeto local funcionando...

---
+++ PHP/editarE.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+	require 'conexao.php';
+	global $pdo;
+
+    $nomeEmpresa = addslashes($_POST['nomeEmpresa']);
+ 
+    if (strlen($_POST['nomeEmpresa']) ==  0){
+        $erros[] = utf8_encode('Preencha o campo nome empresa.');
+    } 
+
+    if (isset($erros) && count($erros) > 0) {
+        
+        $_SESSION['camposForm'] = $_POST;
+        $_SESSION['errosReportados'] = $erros;
+        header("Location: ../VIEW/editar.php");
+
+    }else {
+
+        try {
+            
+            $id = addslashes($_POST['idEmpresas']);
+
+            $stmt = $pdo->prepare('UPDATE empresas SET empresa = :empresa WHERE idEmpresas=:id');
+        
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':empresa', $nomeEmpresa);
+
+            $retorno  = $stmt->execute();
+
+                if (! $retorno){
+                    ?> <script>alert("Erro, tente novamente!");</script> <?php
+                    ?> <script>window.location.href = "../VIEW/editarE.php";</script> <?php
+                }else {
+                    ?> <script>alert("Empresa editado com sucesso!");</script> <?php
+                    ?> <script>window.location.href = "../VIEW/empresas.php";</script> <?php
+                }  
+
+        } catch(PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+
+    }
---
+++ VIEW/editarE.php
@@ -0,0 +1,106 @@
+<?php
+    require_once("../PHP/login.class.php");
+    $login = new Login();
+    $login->verificar("login.php");
+
+    require '../PHP/conexao.php';
+	global $pdo;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+       <meta charset="utf-8"/>
+       <title>Editar Cadastro</title>
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+       <link rel="stylesheet" href="../CSS/forms.css">
+   </head>
+   <body>
+
+        <header>
+            <ul>
+                <li>
+                    <div>
+                        <h1>Sam Crach&aacute;s</h1></li>
+                    </div>
+                <li>
+                    <a href="home.php">Home</a>
+                </li>
+
+                <li>
+                    <a href="lista.php">Funcion&aacute;rios</a>
+                </li>
+
+                <li>
+                    <a class="active" href="empresas.php">Empresas</a>
+                </li>
+
+                <li>
+                    <a href="configuracoes.php">Background</a>
+                </li>
+
+                <li>
+                    <div class="Usuario">
+                        <a> <?php echo $LoginUsuario;?> </a></li>
+                    </div>
+                <li>
+                    <div class="Usuario">
+                         <a href="login.php">Sair</a></li>
+                    </div>
+            </ul>
+		</header>
+
+        <div class=form-criar-crachas>
+            <div><h2>Editar Empresa</h2></div>
+                <div>
+                    <?php
+                    
+                        $id = $_GET['idEmpresas'];       
+
+                        $sql = "SELECT * FROM empresas WHERE idEmpresas = :idEmpresas";
+		                $sql = $pdo->prepare($sql);
+		                $sql->bindValue("idEmpresas", $id);
+                        $sql->execute();
+                        $dados = $sql->fetch(PDO::FETCH_ASSOC);
+
+                        $sql2 = $pdo->query("SELECT * FROM empresas");
+                        $empresas = $sql2->fetchAll(PDO::FETCH_ASSOC);
+                        //echo "<pre>";print_r($empresas);echo "</pre>"; die;
+
+                        if(is_array($_SESSION) && isset($_SESSION['errosReportados'])){
+                            $erros = $_SESSION['errosReportados'];
+                            foreach ($erros as $erro) {
+                                echo $erro;
+                                echo "<br>";
+                            }
+                        }
+
+                        if (is_array($_SESSION) && isset($_SESSION['cadastroRealizado'])){
+                            $sucesso = $_SESSION['cadastroRealizado'];
+                            echo $sucesso;
+                        }
+
+                        if (is_array($_SESSION) && isset($_SESSION['camposForm'])){
+                            $campos = $_SESSION['camposForm'];
+                        }
+                     //   session_unset();
+                    ?>
+            <div class=centro-cadastro>
+                <form id="form_editar" enctype="multipart/form-data"  method="POST" action="../PHP/editarE.php">  
+                
+                    <div>
+                        <input type="hidden" name="idEmpresas" value="<?php echo $id?>">
+                    </div>
+
+                    <div>
+                        <label>Nome Empresa</label><br>
+                        <input class="input" minlength="2" type="text" name="nomeEmpresa" id="inputNome" placeholder="Nome da Empresa" value="<?php echo $dados['empresa']; ?>" required><br>
+                    </div>
+
+                    <div class="btn1">
+                        <button class="input-botao" type="submit">Editar</button>
+                    </div>
+                </form>   
+            <div>
+    </body>
+</html>
