ae32ef58ed6a3c1295cd595e782455848b9bdf8d
Alusem <58275463+Alusem@users.noreply.github.com>
2021-12-04 11:30:55 -0300

    ...

---
+++ crachas/PHP/logar.php
@@ -0,0 +1,29 @@
+<?php
+	require '../PHP/conexao.php';
+	global $pdo;
+
+if (strlen($_POST['senha']) ==  0){
+	session_start();
+	$erros[] = utf8_encode('Preencha o campo senha');
+	$_SESSION['errosReportados'] = $erros;
+	header("Location: ../VIEW/login.php");
+}else{
+	$senhaUser = $_POST['senha'];
+	}
+
+
+if (strlen($_POST['login']) == 0){
+	session_start();
+	$erros[] = utf8_encode('Preencha o campo login');
+	$_SESSION['errosReportados'] = $erros;
+	header("Location: ../VIEW/login.php");
+	}else {
+	$loginUser = $_POST['login'];
+	}
+
+if (strlen($_POST['login']) > 0 && strlen($_POST['senha']) > 0){
+	require_once("login.class.php");		
+	$login = new Login();
+	$login->login_user_params();
+	$logar = $login->logar($loginUser, $senhaUser);
+}
---
+++ crachas/VIEW/abrir_arquivo.php
@@ -0,0 +1,23 @@
+<?php
+    require '../PHP/conexao.php';
+	global $pdo;
+		
+	$id = $_GET['idFuncionario'];
+
+	$stringQuery = "SELECT * FROM crachas WHERE idCrachas = '".$id."';";
+
+	$sql = $pdo->query($stringQuery);
+
+	if($sql->rowCount() > 0){
+		$dados = $sql->fetch();
+	}	
+	
+	$conteudo = $dados['foto'];
+
+	$tipo = 'image/png';
+	
+	ob_clean();
+	header("Content-Type: $tipo");
+	echo $conteudo;
+	
+?>
